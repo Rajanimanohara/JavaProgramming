a101b8b5bf67d6c8132661aa1da75a34f2069b70
Lavanya <Lavanya@Amma>
2018-04-27 10:55:29 +0530

    changes in condition...

--- src/operaterspgm/IncrementDecrement.java
+++ src/operaterspgm/IncrementDecrement.java
@@ -4,10 +4,12 @@ public class IncrementDecrement
 {
 	public static void main(String[] args)
 	{
+		
 		int x=0;
 		int y=0;
 		for(int z=0;z<5;z++)
 		{
+			//condition 
 			if((++x)>2 && (++y)>2)
 			{
 				x++;
